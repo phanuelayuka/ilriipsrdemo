72214d5f2dadb87fa6928ef55cd75a176d3e4fc2
phanuel <ayukaphanuel@gmail.com>
2022-07-10 07:07:49 +0300

    IPSR django project initialization and setup

---
+++ ipsrdemo/urls.py
@@ -0,0 +1,12 @@
+"""ipsrdemo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
