cdc81c070e651d10c54fc0cd67ca689a34fa836a
phanuel <ayukaphanuel@gmail.com>
2022-07-11 09:23:26 +0300

    Added detailed information form step

--- profiles/innovation_submission.py
+++ profiles/innovation_submission.py
@@ -16,20 +16,23 @@ def basic_information(request):
 
 def images_and_ref(request):
     context = {
+        'form_step': 1,
         'images_form': InnovationImageForm(),
         'references_form': InnovationReferenceMaterialUrlForm(),
-        'form_step': 1,
-        'next_form': reverse('profiles:inno-create-detailed')
+        'next_form': reverse('profiles:inno-create-detailed'),
+        'previous_form': reverse('profiles:inno-create-basic')
     }
     return render(request, 'profiles/innovation_form_snippets/images_and_links.html', context=context)
 
 
 def detailed_information(request):
     context = {
+        'form_step': 2,
         'detail_form': DetailedInformationForm(),
-        'contact_persons_form': InnovationContactPersonForm(),
-        'contributors_form': InnovationContributorForm(),
-        'form_step': 2
+        # 'contact_persons_form': InnovationContactPersonForm(),
+        # 'contributors_form': InnovationContributorForm(),
+        'next_form': reverse('profiles:inno-create-detailed'),
+        'previous_form': reverse('profiles:inno-create-doc')
     }
     return render(request, 'profiles/innovation_form_snippets/detailed_information.html', context=context)
 
